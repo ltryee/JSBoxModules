e2d20f9377a619c6918f661cb1efcfe811feacac
vectorliu <vectorliu@tencent.com>
2018-04-08 16:48:30 +0800

    LocationDataSource

--- NaviPortal/scripts/UI/SettingsPanel.js
+++ NaviPortal/scripts/UI/SettingsPanel.js
@@ -1,64 +1,12 @@
 'use strict'
 
 const Config = require('scripts/Core/GlobalConfig.js')
-const PathCache = require('scripts/Core/Cache.js')[Config.cache.pathKey]
-const LocationCache = require('scripts/Core/Cache.js')[Config.cache.locationKey]
-
-const AddLocationView = () => {
-  let view = require('scripts/AddLocationPanel.js')
-  $console.info(view)
-
-  return new Promise((resolve, reject) => {
-    $location.fetch({
-      handler: (resp) => {
-        let lat = resp.lat
-        let lng = resp.lng
-
-        resolve({lat, lng})
-      }
-    })
-  })
-  .then((location) => {
-    view.views[0].props.location = location
-    $console.info(location)
-    $console.info(JSON.stringify(view))
-    return view
-  })
-}
-
-let makeLocations = () => {
-  let list = LocationCache.keys().map((item) => {
-    return {
-      label: {
-        text: item
-      }
-    }
-  }) || []
-
-  list.push({
-    type: 'label',
-    props: {
-      text: '添加',
-      textColor: $color('tint'),
-      align: $align.center
-    },
-    layout: $layout.fill,
-    events: {
-      tapped: (sender) => {
-        AddLocationView().then((view) => {
-          $ui.push(view)
-        })
-      }
-    }
-  })
-
-  return list
-}
+const LocationDataSource = require('scripts/Core/LocationDataSource.js')
 
 let dataSource = [
   {
     title: '地点',
-    rows: makeLocations()
+    rows: LocationDataSource()
   },
   {
     title: '路线',
