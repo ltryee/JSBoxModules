e82e6d17861f173ff491a1f6b152671675889dc5
vectorliu <vectorliu@tencent.com>
2018-04-09 20:24:13 +0800

    quick panel

---
+++ NaviPortal/scripts/UI/GridView.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const DefaultOptions = {
+  colunms: 1, // cell的列数
+  margin: 5,  // cell的边距
+  height: 40, // cell的高度
+}
+
+/**
+ * 生成网格view对象
+ * @param   {?String}     identifier        view的id
+ * @param   {?Dictionary} options           view的配置项
+ * @param   {?Number}     options.colunms   cell列数
+ * @param   {?Number}     options.height    cell高度
+ * @param   {?Number}     options.margin    cell间距
+ * @param   {!Array}      dataList          数据
+ * @param   {!Function}   didSelectCallback 点击事件回调
+ * @returns {Object}      网格view对象
+ */
+let maker = (identifier, options, dataList, didSelectCallback) => {
+  var theOptions = Object.assign({}, DefaultOptions, options)
+  let objID = identifier || 'matrix'
+
+  let dataSource = dataList.map((item, index) => {
+    return {
+      tile: {
+        text: '' + (item.name || item.title || item.id || item),
+        info: item
+      }
+    }
+  })
+
+  let template = [{
+    type: 'label',
+    props: {
+      id: 'tile',
+      align: $align.center,
+      bgcolor: $color('tint'),
+      textColor: $color('white'),
+      radius: theOptions.margin,
+    },
+    layout: $layout.fill
+  }]
+
+  let matrix = {
+    type: "matrix",
+    props: {
+      id: objID,
+      columns: theOptions.colunms,
+      itemHeight: theOptions.height,
+      spacing: theOptions.margin,
+      template: template,
+      data: dataSource
+    },
+    layout: $layout.fill,
+    events: {
+      didSelect: function (sender, indexPath, data) {
+        didSelectCallback(sender, indexPath, data.tile.info)
+      }
+    }
+  }
+
+  return matrix
+}
+
+module.exports = maker
--- NaviPortal/scripts/UI/QuickPanel.js
+++ NaviPortal/scripts/UI/QuickPanel.js
@@ -1,19 +1,25 @@
 'use strict'
 
 const Config = require('scripts/Core/GlobalConfig.js')
+const GridViewMaker = require('scripts/UI/GridView.js')
+const PathCache = require('scripts/Core/Cache.js')[Config.cache.pathKey]
+
+let dataList = Object.values(PathCache.get()).map((item) => {
+  return Object.assign({}, item, {
+    name: `${item.from.name}->${item.to.name}`
+  })
+})
+
+let gridView = GridViewMaker('QuickPanel', {colunms: 2}, dataList, (sender, indexPath, path) => {
+  let URL = `baidumap://map/direction?origin=name:${$text.URLEncode(path.from.name)}|latlng:${path.from.lat},${path.from.lng}&destination=latlng:${path.to.lat},${path.to.lng}|name:${$text.URLEncode(path.to.name)}&mode=driving&coord_type=wgs84`
+  $app.openURL(URL)
+})
 
 let view = {
   props: {
     debugging: Config.debugging
   },
-  views: [{
-    type: 'label',
-    props: {
-      text: 'quick',
-      align: $align.center
-    },
-    layout: $layout.fill,
-  }]
+  views: [gridView]
 }
 
 module.exports = view
